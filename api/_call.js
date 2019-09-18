242a3bff2fb96e91e0fb051381cb88aad88e427d
Razzaru <razzaru@yandex.ru>
2019-09-18 14:21:03 +0300

    Initial commit

---
+++ api/_call.js
@@ -0,0 +1,27 @@
+import fetch from 'isomorphic-unfetch';
+
+const defaultUrl = null;
+
+export default async ({
+  endpoint, headers = {}, method = 'GET', body = {}
+}) => {
+  const stringifiedBody = JSON.stringify(body);
+
+  const postBody = method === 'POST' ? {
+    body: stringifiedBody,
+  } : {};
+
+  const result = await fetch(`${defaultUrl}/${endpoint}`, {
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    ...postBody,
+    method,
+  });
+
+
+  const json = await result.json();
+
+  return json;
+};
---
+++ combined-reducer.js
@@ -0,0 +1,16 @@
+import layoutCombinedReducer from './reducers/index.js'
+import { combineReducers } from 'redux'
+
+let reducers = [layoutCombinedReducer];
+let commonReducer = {};
+
+for(let i = 0; i < reducers.length; ++i) {
+    let properties = Object.keys(reducers[i]);
+    for(let j = 0; j < properties.length; ++j) {
+        commonReducer[properties[j]] = reducers[i][properties[j]];
+    }
+}
+
+const combineReducer = combineReducers(commonReducer);
+
+export default combineReducer;
